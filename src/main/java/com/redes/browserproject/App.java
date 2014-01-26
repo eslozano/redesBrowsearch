4ae7015334e15660d490ab17de5bd5c24310e2ab
eslozano <eslozano@espol.edu.ec>
2014-01-26 15:30:52 -0500

    Inicios del proyecto

---
+++ src/main/java/com/redes/browserproject/App.java
@@ -0,0 +1,15 @@
+package com.redes.browserproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    public static void main( String[] args )
+    {        
+        Browser myBrowser;
+        myBrowser = new Browser();
+    }
+}
